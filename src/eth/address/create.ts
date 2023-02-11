ea579a8565dc2a21dab1ddfbbee61668d9a214e9
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-02-11 23:18:16 +0900

    [CHANGED] create address

--- src/eth/address/create.ts
+++ src/eth/address/create.ts
@@ -1,7 +1,9 @@
 import Web3 from 'web3';
 
-const createAddress = ()=>{
+export const createAddress = ()=>{
   const web3 = new Web3();
 
   const address = web3.eth.accounts.create();
+
+  return address;
 }
