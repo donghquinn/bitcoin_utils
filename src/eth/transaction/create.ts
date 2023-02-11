8c5fdc95f84f51615f9c82b38b24ea658e701dfe
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-02-11 23:39:32 +0900

    wip

--- src/eth/transaction/create.ts
+++ src/eth/transaction/create.ts
@@ -2,13 +2,13 @@ import { web3Base } from "eth/common/base"
 
 
 export const createTransaction = async (fromAddress: string, toAddress: string, value: number) => {
-  const web3 = web3Base()
+  const web3 = web3Base();
 
   const rawTransaction  = {
     to: toAddress,
     from: fromAddress,
     value,
-  }
+  };
 
   const gas = await web3.eth.estimateGas(rawTransaction);
 };
