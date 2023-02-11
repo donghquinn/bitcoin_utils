773d5d94cd440949f325279026f298404dec7cde
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-02-11 23:09:03 +0900

    [CHANGED] license

--- src/transaction/send.mjs
+++ src/transaction/send.mjs
@@ -8,34 +8,38 @@ import fetch from 'node-fetch';
  * @returns
  */
 export const sendToAddress = async (txId, networkType) => {
-  const headers = {
-    authorization: toBasicAuth('testuser', '1234'),
-    'content-type': 'text/plain',
-    Accept: 'text/plain',
-  };
+  try {
+    const headers = {
+      authorization: toBasicAuth('testuser', '1234'),
+      'content-type': 'text/plain',
+      Accept: 'text/plain',
+    };
 
-  const options = {
-    headers,
-    method: 'POST',
-    body: JSON.stringify({
-      id: 'HTTP_FIXED',
-      method: 'sendrawtransaction',
-      jsonrpc: '1.0',
-      params: [txId],
-    }),
-  };
+    const options = {
+      headers,
+      method: 'POST',
+      body: JSON.stringify({
+        id: 'HTTP_FIXED',
+        method: 'sendrawtransaction',
+        jsonrpc: '1.0',
+        params: [txId],
+      }),
+    };
 
-  // // const quickNodeUrl = `${process.env.TESTNET_URL}/${process.env.TESTNET_TOKEN}`;
-  let nodeUrl;
+    // // const quickNodeUrl = `${process.env.TESTNET_URL}/${process.env.TESTNET_TOKEN}`;
+    let nodeUrl;
 
-  if (networkType === "main") { nodeUrl = process.env.MAINNET_NODE; }
-  if (networkType === "test") { nodeUrl = process.env.TESTNET_NODE; }
+    if (networkType === "main") { nodeUrl = process.env.MAINNET_NODE; }
+    if (networkType === "test") { nodeUrl = process.env.TESTNET_NODE; }
 
-  const response = await (await fetch(nodeUrl, options)).json();
+    const response = await (await fetch(nodeUrl, options)).json();
 
-  console.log('Send To ToAddress Response: %o', {
-    response,
-  });
+    console.log('Send To ToAddress Response: %o', {
+      response,
+    });
 
-  return response;
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
