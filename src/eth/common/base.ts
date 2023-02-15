8dac9f30a3449bf98b7e3334292b6170b445bded
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-02-15 14:06:56 +0900

    changed: timeout

--- src/eth/common/base.ts
+++ src/eth/common/base.ts
@@ -1,7 +1,8 @@
 import Web3 from 'web3';
 
 export const web3Base = () => {
-  const web3 = new Web3(new Web3.providers.HttpProvider("https://nebtc-test-rpc.zetra.kr"));
+  const rpcUrl = process.env.ETH_RPC_URL!
+  const web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl, {timeout: 30000}));
 
   return web3;
 }
